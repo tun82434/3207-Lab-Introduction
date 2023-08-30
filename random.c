9408f5dd7f20c85cfe168b34a8da51cb4689a585
unknown <tun82434>
2023-08-30 19:11:25 -0400

    Update print_random.c

--- random.c
+++ random.c
@@ -1,3 +1,13 @@
+#include "random.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
+
+char randchar(){
+    srand(time(NULL));
+    int charNum = rand() % 26;
+    return ('a' + charNum);
+
+}
+
